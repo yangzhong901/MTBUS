d4b5c0d8e179c726e6b1bb0e8838b395d2199cad
yangzhong901 <43029111+yangzhong901@users.noreply.github.com>
2024-08-02 22:26:06 +0800

    Update and rename cudaTest.cpp to Main.cpp

--- GPUSearch/Main.cpp
+++ GPUSearch/Main.cpp
@@ -7,13 +7,6 @@ int main(int argc, char* argv[])
 {  
     std::clock_t start, end;
 
-    //使用GPU device 0: 
-    //SM的数量：10
-    //每个线程块的共享内存大小：48 KB
-    //每个线程块的最大线程数：1024
-    //每个SM的最大线程数：2048
-    //每个SM的最大线程束数：64
-    
     if (atoi(argv[8])== 0)
     {
         std::cout << "GPU Running:\n";
